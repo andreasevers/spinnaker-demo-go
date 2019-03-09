717454a4ae17593bfed9f6789782b5e2a66e7d1b
Andreas Evers <andreas.evers@rabobank.nl>
2019-03-09 21:38:16 +0100

    Initial commit

---
+++ spinnaker-demo.go
@@ -0,0 +1,53 @@
+package main
+
+import (
+    "log"
+    "net/http"
+    "io/ioutil"
+    "github.com/robfig/cron"
+)
+
+type MyHandler struct {
+}
+
+func (this *MyHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+    log.Print("Meeseeks")
+    var path string
+    path = r.URL.Path[1:]
+    if path == "" {
+        path = "demo.html"
+    }
+    data, err := ioutil.ReadFile(string(path))
+
+    if err == nil {
+        w.Write(data)
+    } else {
+        w.WriteHeader(404)
+        w.Write([]byte("404 Something went wrong - " + http.StatusText(404)))
+    }
+}
+
+func logRickAndMortyNames() {
+    log.Print("Rick")
+    log.Print("Morty")
+    log.Print("Beth")
+    log.Print("Jerry")
+    log.Print("Summer")
+    log.Print("Mr. Poopybutthole")
+    log.Print("Mr. Meeseeks")
+    log.Print("Birdperson")
+    log.Print("Ants-In-My-Eyes Johnson")
+    log.Print("Jessica")
+    log.Print("Gazorpazorpfield",)
+    log.Print("Noob Noob")
+    log.Print("Scary Terry")
+    log.Print("Squanchy")
+}
+
+func main() {
+    c := cron.New()
+    c.AddFunc("@every 20s", logRickAndMortyNames)
+    c.Start()
+    http.Handle("/", new(MyHandler))
+    log.Fatal(http.ListenAndServe(":8080", nil))
+}
